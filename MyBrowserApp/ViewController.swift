62a65bd17a9cbf6e645c112df2ee237dfce97bb2
naoki-kkc <naoki2kuchi@gmail.com>
2021-03-21 00:53:40 +0900

    Initial Commit

---
+++ MyBrowserApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBrowserApp
+//
+//  Created by ginger893 on 2021/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
