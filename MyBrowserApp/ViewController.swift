bdd791772b8b979d965d69e25d0a3f8fb5faed92
naoki-kkc <naoki2kuchi@gmail.com>
2021-03-23 23:50:22 +0900

    lesson 04 end

--- MyBrowserApp/ViewController.swift
+++ MyBrowserApp/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var browserWebView: WKWebView!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var fowardButton: UIBarButtonItem!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func goBack(_ sender: Any) {
+    }
+    @IBAction func goFoward(_ sender: Any) {
+    }
+    
+    @IBAction func reload(_ sender: Any) {
+    }
+    
 }
 
