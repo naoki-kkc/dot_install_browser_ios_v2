ac4d81d2631fb0de1c2f5e0b9727fe931e058f71
naoki-kkc <naoki2kuchi@gmail.com>
2021-03-25 22:57:00 +0900

    lesson 06 end

--- MyBrowserApp/ViewController.swift
+++ MyBrowserApp/ViewController.swift
@@ -18,10 +18,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "http://dotinstall.com"
-        let urlRequest = URLRequest(url: URL(string: urlString)!)
-        self.browserWebView.load(urlRequest)
+//        let urlString = "http://dotinstall.com"
+        let urlString = ""
+        self.loadUrl(urlString: urlString)
     }
+    
+    func getValidatedUrl(urlString: String) -> URL?{
+        if URL(string: urlString) == nil{
+            print("invalid url")
+            return nil
+        }
+        return URL(string: urlString)
+    }
+    
+    func loadUrl(urlString: String){
+        if let url = self.getValidatedUrl(urlString: urlString){
+            let urlRequest = URLRequest(url: url)
+            self.browserWebView.load(urlRequest)
+        }
+        
+    }
+    
     @IBAction func goBack(_ sender: Any) {
     }
     @IBAction func goFoward(_ sender: Any) {
